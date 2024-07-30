501c62a0a3710e2a7fe4554562e46b124efac354
Lubotech <luboyerajoel573@gmail.com>
2024-07-31 02:18:25 +0300

    Worked on TodoLists, arrays and loops

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -18,18 +18,18 @@ function renderTodoList() {
     //const dueDate =todoObject.dueDate;
     const {name, dueDate} = todoObject;
     const html = `
-    <p>
-      ${name} ${dueDate} 
-      <button onclick="
-        todoList.splice(${i}, i);
-        renderTodoList();
-      ">Delete</button>
-    </p>
+    <div> ${name} </div> 
+    <div> ${dueDate} </div> 
+    <button onclick="
+      todoList.splice(${i}, 1);
+      renderTodoList();
+    " class="delete-todo-button">Delete</button>
+  
     `;
 
     todoListHTML += html;
   }
-  console.log(todoListHTML);
+  //console.log(todoListHTML);
 
   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
@@ -39,8 +39,16 @@ function addTodo() {
  const name = inputElement.value;
  //console.log(name);
 
- todoList.push(name);
- console.log(todoList);
+ const dateInputElement = document.querySelector('.js-due-date-input');
+ const dueDate = dateInputElement.value;
+
+ todoList.push({
+  //  name: name,
+  //  dueDate: dueDate,
+  name,
+  dueDate,
+  });
+ //console.log(todoList);
 
  inputElement.value = '';
 
